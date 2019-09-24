f86c1901ee6a76c1f3b29b661e8191da69b58be2
Oghogho Odemwingie <odemwingieog@gmail.com>
2019-09-24 15:58:42 +0100

    changed a few things in hello/index.js to better conform to raml-spec/hello/api.raml

--- hello/src/index.js
+++ hello/src/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/resthub', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/messagehub', { useNewUrlParser: true });
 
 let db = mongoose.connection
 if (!db) {
@@ -24,6 +24,6 @@ if (!db) {
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World with express and nodemon'));
-app.use('/api', apiRoutes);
+app.use('/message', apiRoutes);
 
 app.listen(port, () => console.log(`Running inspiration on port ${ port }`));
